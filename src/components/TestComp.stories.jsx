58d5b25d46fe590f6efe5882ed08b244b587ea65
Anish George <anishgeo@thoughtworks.com>
2023-06-22 13:57:40 +0530

    Remove sample code

--- src/components/TestComp.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { TestComp } from "./TestComp";
-
-export default {
-  title: "TestComp",
-  component: TestComp,
-};
-
-export const Primary = () => <TestComp />;
