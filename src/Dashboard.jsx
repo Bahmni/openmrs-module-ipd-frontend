bf24f6cdcf707a20c2037b645e4c7ede06e3a892
Anish George <anishgeo@thoughtworks.com>
2023-06-28 18:36:10 +0530

    Confirm that styles are working

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Dashboard.scss";
 
 export default function Dashboard(props) {
   return (
     <>
       <h3>Hello, this is IPD</h3>
       <div>Patient UUID: {props.options?.patient?.uuid}</div>
+      <p className="sub-text">Right, some changes</p>
     </>
   );
 }
