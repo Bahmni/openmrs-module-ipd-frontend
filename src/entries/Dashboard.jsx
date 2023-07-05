691c96d48ff3b793c99504dfcd064a52929eb571
Anish George <anishgeo@thoughtworks.com>
2023-07-05 12:47:27 +0530

    Update to new host interface

--- src/entries/Dashboard.jsx
+++ src/entries/Dashboard.jsx
@@ -8,14 +8,14 @@ import { Button } from "carbon-components-react";
  * NOTE: Currently there is nothing in this dashboard, all debugging items
  */
 export default function Dashboard(props) {
+  const { hostData, hostApi } = props;
   return (
     <>
       <h3>IPD Care and Monitoring Dashboard</h3>
       <div>
         <h4>DEBUG: Data received from host</h4>
         <p>
-          <b className="sub-text">Patient UUID:</b>{" "}
-          {props.options?.patient?.uuid}
+          <b className="sub-text">Patient UUID:</b> {hostData?.patient?.uuid}
         </p>
       </div>
       <hr />
@@ -36,7 +36,7 @@ export default function Dashboard(props) {
         </h4>
         <Button
           kind="primary"
-          onClick={() => props.hostInterface.onConfirm?.("event-from-ipd")}
+          onClick={() => hostApi.onConfirm?.("event-from-ipd")}
         >
           Click to send event to host appliation
         </Button>
@@ -46,12 +46,12 @@ export default function Dashboard(props) {
 }
 
 Dashboard.propTypes = {
-  options: PropTypes.shape({
+  hostData: PropTypes.shape({
     patient: PropTypes.shape({
       uuid: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
-  hostInterface: PropTypes.shape({
+  hostApi: PropTypes.shape({
     onConfirm: PropTypes.func,
   }),
 };
