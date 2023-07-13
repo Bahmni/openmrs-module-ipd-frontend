c37634bc6e9b9a30d68a767c9b9c79ab673c0d79
Arjun-Go <arjun.g@thoughtworks.com>
2023-07-13 19:31:01 +0530

    update. drug chart modal with necessary fields and styling

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,30 @@
 // Pick up output.publicPath from webpack.config.js. Maps to the proxy setting when using the micro-frontend capability
 /* global __webpack_public_path__:writable */
 export const BASE_URL = __webpack_public_path__ || "/";
-
 export const LS_LANG_KEY = "NG_TRANSLATE_LANG_KEY";
+
+export const enableScheduleFrequencies = [
+  {
+    name: "Twice a day",
+    frequencyPerDay: 2,
+  },
+  {
+    name: "Thrice a day",
+    frequencyPerDay: 3,
+  },
+  {
+    name: "Four times a day",
+    frequencyPerDay: 4,
+  },
+];
+
+export const enableStartTimeFrequencies = [
+  "Every Hour",
+  "Every 2 hours",
+  "Every 3 hours",
+  "Every 4 hours",
+  "Every 6 hours",
+  "Every 8 hours",
+  "Every 12 hours",
+  "Once a day",
+];
--- src/entries/DrugChartModal/DrugChartModal.jsx
+++ src/entries/DrugChartModal/DrugChartModal.jsx
@@ -1,35 +1,185 @@
-import React from "react";
+import {
+  DatePickerCarbon,
+  DropdownCarbon,
+  NumberInputCarbon,
+  TimePicker,
+} from "bahmni-carbon-ui";
+import { Modal, TextArea, TextInput } from "carbon-components-react";
+import moment from "moment";
 import PropTypes from "prop-types";
-import "./DrugChartModal.scss";
-// import { TestComp } from "bahmni-carbon-ui";
-// import { Button } from "carbon-components-react";
+import React from "react";
+import { FormattedMessage } from "react-intl";
 import { I18nProvider } from "../../features/i18n/I18nProvider";
-// import { FormattedMessage } from "react-intl";
+import "./DrugChartModal.scss";
+import {
+  enableScheduleFrequencies,
+  enableStartTimeFrequencies,
+} from "../../constants";
 
-/**
- * NOTE: Currently there is nothing in this dashboard, all debugging items
- */
 export default function DrugChartModal(props) {
-  const { hostData, hostApi } = props;
+  let { hostData } = props;
+  const enableScheduleFrequency = enableScheduleFrequencies.find(
+    (frequency) =>
+      frequency.name === hostData?.drugOrder?.uniformDosingType?.frequency
+  );
+  const enableStartTime = enableStartTimeFrequencies.includes(
+    hostData?.drugOrder?.uniformDosingType?.frequency
+  );
+
   return (
-    <>
-      <I18nProvider>
-        <h2>Add to Drug Chart</h2>
+    <I18nProvider>
+      <Modal
+        open
+        modalHeading={<FormattedMessage id="DRUG_CHART_MODAL_HEADER" />}
+        primaryButtonText={<FormattedMessage id="MODAL_SAVE" />}
+        secondaryButtonText={<FormattedMessage id="MODAL_CANCEL" />}
+        onRequestClose={() => {
+          if (hostData) {
+            hostData.close = false;
+          }
+        }}
+        onRequestSubmit={() => {}}
+        onSecondarySubmit={() => {}}
+      >
+        {console.log(hostData)}
         <div>
-          <h3>Hello</h3>
-          <h3>{hostData ? "Yes" : "No"}</h3>
-          <h3>{hostApi ? "Yes" : "No"}</h3>
+          <TextInput
+            className="drug-name"
+            readOnly
+            type="text"
+            value={hostData?.drugOrder?.drug?.name}
+            labelText="Drug Name"
+          />
+          <div className="inline-field">
+            <NumberInputCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              label={"Dose"}
+              value={hostData?.drugOrder?.uniformDosingType?.dose}
+              isDisabled={true}
+            />
+            <DropdownCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              titleText={"Units(s)"}
+              selectedValue={hostData?.drugOrder?.uniformDosingType?.doseUnits}
+              options={[]}
+              isDisabled={true}
+            />
+            <NumberInputCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              label={"Duration"}
+              value={hostData?.drugOrder?.duration}
+              isDisabled={true}
+            />
+            <DropdownCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              titleText={"Units(s)"}
+              selectedValue={hostData?.drugOrder?.durationUnit}
+              options={[]}
+              isDisabled={true}
+            />
+          </div>
+          <div className="inline-field">
+            <DropdownCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              titleText={"Frequency"}
+              selectedValue={hostData?.drugOrder?.uniformDosingType?.frequency}
+              options={[]}
+              isDisabled={true}
+            />
+            <DropdownCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              titleText={"Route"}
+              selectedValue={hostData?.drugOrder?.route}
+              options={[]}
+              isDisabled={true}
+            />
+            <DatePickerCarbon
+              id={"Dropdown"}
+              onChange={() => {}}
+              titleText={"Start Date"}
+              minDate={moment().format("MM-DD-YYYY")}
+              title={"Start Date"}
+              value={moment(hostData?.drugOrder?.scheduledDate).format(
+                "MM/DD/YYYY"
+              )}
+              isDisabled={true}
+            />
+          </div>
+          <TextArea
+            className="instruction"
+            readOnly
+            type="text"
+            rows={1}
+            disabled={true}
+            value={hostData?.drugOrder?.instructions || "No Instructions"}
+            labelText="Instruction"
+          />
+          <TextArea
+            className="additional-instruction"
+            readOnly
+            type="text"
+            rows={1}
+            disabled={true}
+            value={
+              hostData?.drugOrder?.additionalInstructions ||
+              "No Additional Instructions"
+            }
+            labelText="Additional Instruction"
+          />
+          <TextArea
+            data-modal-primary-focus
+            className="notes-section"
+            type="text"
+            rows={2}
+            labelText="Notes"
+          />
+          {enableScheduleFrequency && (
+            <div className="schedule-section">
+              {/* <label className="bx--label">Schedule</label> */}
+              <div className="inline-field" id="schedule">
+                {Array.from(
+                  { length: enableScheduleFrequency.frequencyPerDay },
+                  (_, index) => (
+                    <TimePicker
+                      key={index}
+                      onChange={() => {}}
+                      id={`schedule-${index}`}
+                      labelText={`Schedule ${index + 1}`}
+                      defaultTime={moment()}
+                      isRequired={true}
+                    />
+                  )
+                )}
+              </div>
+            </div>
+          )}
+          {enableStartTime && (
+            <div className="start-time">
+              <TimePicker
+                id={"start-time"}
+                onChange={() => {}}
+                defaultTime={moment()}
+                labelText={"Start Time"}
+                isRequired={true}
+              />
+            </div>
+          )}
         </div>
-      </I18nProvider>
-    </>
+      </Modal>
+    </I18nProvider>
   );
 }
 
 DrugChartModal.propTypes = {
   hostData: PropTypes.shape({
-    patient: PropTypes.shape({
-      uuid: PropTypes.string.isRequired,
-    }).isRequired,
+    drugOrder: PropTypes.object,
+    close: PropTypes.bool,
   }).isRequired,
   hostApi: PropTypes.shape({
     onConfirm: PropTypes.func,
