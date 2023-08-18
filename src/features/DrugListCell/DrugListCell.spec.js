c973446d09a51c8885a5589af2073fc267b796f3
Tanya Aggarwal <tanya.aggarwal@thoughtworks.com>
2023-08-18 18:52:50 +0530

    [Sowmya|Tanya] refactor drug chart tests

--- src/features/DrugListCell/DrugListCell.spec.js
+++ src/features/DrugListCell/DrugListCell.spec.js
@@ -1,43 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import DrugListCell from "./DrugListCell";
+import {
+  testDrugInfo,
+  testDrugInfoWithAdministeredLateStatus,
+  testDrugInfoWithAdministeredStatus,
+} from "./DrugListCellMockData";
 
-const testDrugInfo = {
-  drugName: "Suxamethonium Chloride 100 mg/2 mL Ampoule (50 mg/mL )",
-  drugRoute: "Intravenous",
-  administrationInfo: [],
-  duration: "4 Day(s)",
-  dosage: 2,
-  doseType: "mg",
-};
-
-const testDrugInfoWithAdministeredLateStatus = {
-  drugName: "Enalapril 5 mg Tablet",
-  drugRoute: "Oral",
-  administrationInfo: [
-    {
-      time: 8.45,
-      kind: "Administered-Late",
-    },
-  ],
-  duration: "4 Day(s)",
-  dosage: 1,
-  doseType: "Tablet(s)",
-};
-
-const testDrugInfoWithAdministeredStatus = {
-  drugName: "Enalapril 5 mg Tablet",
-  drugRoute: "Oral",
-  administrationInfo: [
-    {
-      time: 8.45,
-      kind: "Administered",
-    },
-  ],
-  duration: "4 Day(s)",
-  dosage: 1,
-  doseType: "Tablet(s)",
-};
 describe("DrugListCell", () => {
   it("should  match snapshot", () => {
     const { container } = render(<DrugListCell drugInfo={testDrugInfo} />);
