104d7d52dad48818603665db6b9d01b6eab34aa1
Arjun-Go <arjun.g@thoughtworks.com>
2023-10-26 11:56:24 +0530

    add. tests for treatments section

---
+++ src/features/DisplayControls/Treatments/Treatments.spec.jsx
@@ -0,0 +1,104 @@
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import Treatments from "./Treatments";
+import { getPrescribedAndActiveDrugOrders } from "./TreatmentsUtils";
+
+jest.mock("./TreatmentsUtils", () => ({
+  getPrescribedAndActiveDrugOrders: jest.fn(),
+}));
+
+describe("Treatments", () => {
+  beforeEach(() => {
+    getPrescribedAndActiveDrugOrders.mockImplementation(() => {
+      return Promise.resolve({
+        visitDrugOrders: [],
+        activeDrugOrders: [],
+      });
+    });
+  });
+
+  it("should show no treatments message if no drug orders are present for that patient", async () => {
+    getPrescribedAndActiveDrugOrders.mockResolvedValueOnce({
+      visitDrugOrders: [],
+      activeDrugOrders: [],
+    });
+    const { getByText } = render(<Treatments />);
+    await waitFor(() => {
+      expect(
+        getByText("No treatments prescribed for this patient")
+      ).toBeTruthy();
+    });
+  });
+
+  it("should show OPD treatments without Add to Drug Chart link", async () => {
+    const treatments = [
+      {
+        uuid: "1",
+        effectiveStartDate: new Date("01/01/2022"),
+        drug: {
+          name: "Drug 1",
+        },
+        dosingInstructions: {
+          dose: 1,
+          doseUnits: "mg",
+          route: "Oral",
+          frequency: "Once a day",
+        },
+        duration: 7,
+        durationUnits: "days",
+        provider: {
+          name: "Dr. John Doe",
+        },
+        careSetting: "OUTPATIENT",
+      },
+    ];
+    getPrescribedAndActiveDrugOrders.mockResolvedValueOnce({
+      visitDrugOrders: treatments,
+      activeDrugOrders: [],
+    });
+    const { getByText, queryByText } = render(<Treatments />);
+    await waitFor(() => {
+      expect(getByText("1/1/2022")).toBeTruthy();
+      expect(getByText("Drug 1")).toBeTruthy();
+      expect(getByText("1 mg - Oral - Once a day - for 7 days")).toBeTruthy();
+      expect(getByText("Dr. John Doe")).toBeTruthy();
+      expect(queryByText("Add to Drug Chart")).toBeNull();
+    });
+  });
+
+  it("should render an AddToDrugChart link for inpatient treatments", async () => {
+    const treatments = [
+      {
+        uuid: "1",
+        effectiveStartDate: new Date("01/01/2022"),
+        drug: {
+          name: "Drug 1",
+        },
+        dosingInstructions: {
+          dose: 1,
+          doseUnits: "mg",
+          route: "Oral",
+          frequency: "Once a day",
+        },
+        duration: 7,
+        durationUnits: "days",
+        provider: {
+          name: "Dr. John Doe",
+        },
+        careSetting: "INPATIENT",
+      },
+    ];
+    getPrescribedAndActiveDrugOrders.mockResolvedValueOnce({
+      visitDrugOrders: treatments,
+      activeDrugOrders: [],
+    });
+    const { getByText } = render(<Treatments />);
+    await waitFor(() => {
+      expect(getByText("1/1/2022")).toBeTruthy();
+      expect(getByText("Drug 1")).toBeTruthy();
+      expect(getByText("1 mg - Oral - Once a day - for 7 days")).toBeTruthy();
+      expect(getByText("Dr. John Doe")).toBeTruthy();
+      expect(getByText("Add to Drug Chart")).toBeTruthy();
+    });
+  });
+});
