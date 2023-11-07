e8fad9c9d1d5909aba1512e4d905aa4b00194215
tanyaa-tw <120625142+tanyaa-tw@users.noreply.github.com>
2023-11-07 10:29:40 +0530

    [Tanya] add the drug chart slider on clicking add to drug chart button

--- src/features/DisplayControls/Treatments/components/Treatments.jsx
+++ src/features/DisplayControls/Treatments/components/Treatments.jsx
@@ -9,6 +9,7 @@ import {
   TableRow,
   Link,
   DataTableSkeleton,
+  Button,
 } from "carbon-components-react";
 import { FormattedMessage } from "react-intl";
 import { useState } from "react";
@@ -16,15 +17,20 @@ import PropTypes from "prop-types";
 import {
   getPrescribedAndActiveDrugOrders,
   treatmentHeaders,
+  getConfigsForTreatments,
 } from "../utils/TreatmentsUtils";
 import "../styles/Treatments.scss";
 import { formatDate } from "../../../../utils/DateFormatter";
 import { dateFormat } from "../../../../constants";
+import DrugChartSlider from "./DrugChartSlider/DrugChartSlider";
 
 const Treatments = (props) => {
   const { patientId } = props;
   const [treatments, setTreatments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [buttonClicked, setButtonClick] = useState(false);
+  const [hostData, setHostData] = useState({});
+  const [drugOrders, setDrugOrders] = useState([]);
 
   const NoTreatmentsMessage = (
     <FormattedMessage
@@ -32,6 +38,18 @@ const Treatments = (props) => {
       defaultMessage={"No IPD Medication is prescribed for this patient yet"}
     />
   );
+
+  const handleAddToDrugChartClick = (drugOrderId) => {
+    console.log("drugOrder", drugOrders);
+    console.log("drugOrderId", drugOrderId);
+    // const drugOrderObject = drugOrders.find(
+    //   (drugOrder) => drugOrder.uuid === drugOrderId
+    // );
+    // setHostData(
+    //   drugOrder= drugOrderObject
+    //   );
+    setButtonClick(true);
+  };
 
   const AddToDrugChart = (
     <FormattedMessage
@@ -73,26 +91,60 @@ const Treatments = (props) => {
           drugName: drugOrder.drug.name,
           dosageDetails: setDosingInstructions(drugOrder),
           prescribedBy: drugOrder.provider.name,
-          actions: <Link href="#">{AddToDrugChart}</Link>,
+          // actions: <Link href="#">{AddToDrugChart}</Link>,
+          actions: (
+            <Button
+              as={Link}
+              onClick={() => handleAddToDrugChartClick(drugOrder.uuid)}
+            >
+              {AddToDrugChart}
+            </Button>
+          ),
         };
       });
     setTreatments(treatments);
   };
 
   useEffect(() => {
-    const getActiveDrugOrders = async () => {
-      const drugOrders = await getPrescribedAndActiveDrugOrders(patientId);
-      if (drugOrders.visitDrugOrders) {
-        modifyTreatmentData(drugOrders);
-        setIsLoading(false);
-      }
-    };
+    const getActiveDrugOrdersAndTreatmentConfig = async () => {
+      const drugOrderList = await getPrescribedAndActiveDrugOrders(patientId);
+      const treatmentsConfigList = await getConfigsForTreatments();
 
-    getActiveDrugOrders();
+      console.log("treatmentsConfigList", treatmentsConfigList);
+
+      if (drugOrderList.visitDrugOrders && treatmentsConfigList) {
+        console.log("inside if for treatment config ");
+        setDrugOrders(drugOrderList.visitDrugOrders);
+        modifyTreatmentData(drugOrderList);
+        setHostData({
+          patientId: patientId,
+          scheduleFrequencies: treatmentsConfigList.scheduleFrequencies,
+          startTimeFrequencies: treatmentsConfigList.startTimeFrequencies,
+          enable24HrTimeFormat: treatmentsConfigList.enable24HrTimeFormat,
+          drugOrder: null,
+        });
+      }
+      console.log("drugOrdersList", drugOrderList);
+    };
+    getActiveDrugOrdersAndTreatmentConfig();
+    console.log("treatments", treatments);
   }, []);
+
+  useEffect(() => {
+    if (drugOrders.length > 0) {
+      setIsLoading(false);
+    }
+  }, [drugOrders]);
 
   return (
     <>
+      {buttonClicked && (
+        <DrugChartSlider
+          title="Add to drug chart"
+          hostData={hostData}
+          hostApi="api"
+        />
+      )}
       {isLoading ? (
         <DataTableSkeleton />
       ) : treatments && treatments.length === 0 ? (
