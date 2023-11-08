f2a45ab2e6df72a6e9cc48cdfced4e546a786a21
Phanindra-tw <v.tadikonda@thoughtworks.com>
2023-11-08 15:53:18 +0530

    Phani | Move Patient Dashboard Url into common utils

--- src/entries/Dashboard/Dashboard.jsx
+++ src/entries/Dashboard/Dashboard.jsx
@@ -16,7 +16,7 @@ import "./Dashboard.scss";
 import data from "../../utils/config.json";
 import PropTypes from "prop-types";
 import { I18nProvider } from "../../features/i18n/I18nProvider";
-import { getPatientDashboardUrl } from "../../utils/DashboardUtils/DashboardUtils";
+import { getPatientDashboardUrl } from "../../utils/CommonUtils";
 
 export default function Dashboard(props) {
   const { hostData } = props;
