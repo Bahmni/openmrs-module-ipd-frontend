be088fdb77396a4d404605ae4d6e5a1efba6d9a9
sowmya198 <sowmya.ayilam.s@gmail.com>
2023-11-08 18:06:59 +0530

    adds test for nursing tasks component

---
+++ src/features/DisplayControls/NursingTasks/tests/NursingTasks.spec.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import NursingTasks from "../Components/NursingTasks";
+import { mockNursingTasksResponse } from "./NursingTasksUtilsMockData";
+
+const mockFetchMedicationNursingTasks = jest.fn();
+
+jest.mock("../utils/NursingTasksUtils", () => {
+  const originalModule = jest.requireActual("../utils/NursingTasksUtils");
+  return {
+    ...originalModule,
+    fetchMedicationNursingTasks: () => mockFetchMedicationNursingTasks(),
+  };
+});
+
+describe("NursingTasks", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should show loading state", async () => {
+    const { getByText } = render(<NursingTasks patientId="patientid" />);
+
+    await waitFor(() => {
+      expect(getByText("Loading...")).toBeTruthy();
+    });
+  });
+
+  it("should show no nursing tasks message when nursing task reponse is empty", async () => {
+    mockFetchMedicationNursingTasks.mockResolvedValueOnce([]);
+    const { getByText } = render(<NursingTasks patientId="patientid" />);
+
+    await waitFor(() => {
+      expect(mockFetchMedicationNursingTasks).toHaveBeenCalledTimes(1);
+      expect(
+        getByText("No nursing task is scheduled for the patient")
+      ).toBeTruthy();
+    });
+  });
+
+  it("should show nursing tasks when nursing task reponse is not empty", async () => {
+    mockFetchMedicationNursingTasks.mockResolvedValueOnce(
+      mockNursingTasksResponse
+    );
+    const { getByText, asFragment } = render(
+      <NursingTasks patientId="patientid" />
+    );
+    await waitFor(() => {
+      expect(mockFetchMedicationNursingTasks).toHaveBeenCalledTimes(1);
+      expect(
+        getByText("Paracetamol 120 mg/5 mL Suspension (Liquid)")
+      ).toBeTruthy();
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
