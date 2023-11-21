9f30a015caffd60c1560f1cc09bee311906a7540
Arjun-Go <arjun.g@thoughtworks.com>
2023-11-21 18:06:06 +0530

    update. nursing task extraction logic

--- src/features/DisplayControls/NursingTasks/tests/NursingTasksUtilsMockData.js
+++ src/features/DisplayControls/NursingTasks/tests/NursingTasksUtilsMockData.js
@@ -9,30 +9,29 @@ export const mockResponse = {
     patient: {
       uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
     },
-    order: {
-      drug: {
-        display: "Paracetamol 120 mg/5 mL Suspension (Liquid)",
-      },
-      route: {
-        uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
-        display: "Oral",
-      },
-      dose: 25,
-      doseUnits: {
-        uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
-        display: "ml",
-      },
-      duration: 3,
-      durationUnits: {
-        uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
-        display: "Day(s)",
-      },
-    },
     slots: [
       {
         id: 1,
         uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
-        orderId: 11,
+        order: {
+          drug: {
+            display: "Paracetamol 120 mg/5 mL Suspension (Liquid)",
+          },
+          route: {
+            uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
+            display: "Oral",
+          },
+          dose: 25,
+          doseUnits: {
+            uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
+            display: "ml",
+          },
+          duration: 3,
+          durationUnits: {
+            uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
+            display: "Day(s)",
+          },
+        },
         serviceType: "MedicationRequest",
         status: "SCHEDULED",
         startTime: 1690906550,
@@ -52,33 +51,32 @@ export const mockNursingTasksResponse = [
     patient: {
       uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
     },
-    order: {
-      drug: {
-        display: "Paracetamol 120 mg/5 mL Suspension (Liquid)",
-      },
-      route: {
-        uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
-        display: "Oral",
-      },
-      dose: 25,
-      doseUnits: {
-        uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
-        display: "mg",
-      },
-      duration: 3,
-      durationUnits: {
-        uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
-        display: "Day(s)",
-      },
-    },
     slots: [
       {
         id: 1,
         uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
-        orderId: 11,
         serviceType: "MedicationRequest",
         status: "SCHEDULED",
         startTime: 1690906550,
+        order: {
+          drug: {
+            display: "Paracetamol 120 mg/5 mL Suspension (Liquid)",
+          },
+          route: {
+            uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
+            display: "Oral",
+          },
+          dose: 25,
+          doseUnits: {
+            uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
+            display: "mg",
+          },
+          duration: 3,
+          durationUnits: {
+            uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
+            display: "Day(s)",
+          },
+        },
       },
     ],
   },
@@ -92,38 +90,55 @@ export const mockNursingTasksResponse = [
     patient: {
       uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
     },
-    order: {
-      drug: {
-        display: "Paracetamol 120 mg (Tablet)",
-      },
-      route: {
-        uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
-        display: "Oral",
-      },
-      dose: 25,
-      doseUnits: {
-        uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
-        display: "mg",
-      },
-      duration: 3,
-      durationUnits: {
-        uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
-        display: "Day(s)",
-      },
-    },
     slots: [
       {
         id: 1,
         uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
-        orderId: 11,
         serviceType: "MedicationRequest",
         status: "SCHEDULED",
         startTime: 1690906550,
+        order: {
+          drug: {
+            display: "Paracetamol 120 mg (Tablet)",
+          },
+          route: {
+            uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
+            display: "Oral",
+          },
+          dose: 25,
+          doseUnits: {
+            uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
+            display: "mg",
+          },
+          duration: 3,
+          durationUnits: {
+            uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
+            display: "Day(s)",
+          },
+        },
       },
       {
         id: 2,
         uuid: "738aa77d-03fc-438f-a87a-ae8a8867c421",
-        orderId: 11,
+        order: {
+          drug: {
+            display: "Paracetamol 120 mg (Tablet)",
+          },
+          route: {
+            uuid: "9d6bc13f-3f10-11e4-adec-0800271c1b75",
+            display: "Oral",
+          },
+          dose: 25,
+          doseUnits: {
+            uuid: "86239663-7b04-4563-b877-d7efc4fe6c46",
+            display: "mg",
+          },
+          duration: 3,
+          durationUnits: {
+            uuid: "9d7437a9-3f10-11e4-adec-0800271c1b75",
+            display: "Day(s)",
+          },
+        },
         serviceType: "MedicationRequest",
         status: "SCHEDULED",
         startTime: 1690907550,
