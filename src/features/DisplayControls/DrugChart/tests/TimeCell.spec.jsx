234091d24dcde696829b6f8691c627632c56e836
ayush34957 <ayush.sharma1@thoughtworks.com>
2023-12-19 20:27:49 +0530

    test: added tests - Ayush

--- src/features/DisplayControls/DrugChart/tests/TimeCell.spec.jsx
+++ src/features/DisplayControls/DrugChart/tests/TimeCell.spec.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TimeCell from "../components/TimeCell";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("TimeCell", () => {
   it("should render icon on left side of cell if minutes is less than 30", () => {
@@ -11,5 +12,55 @@ describe("TimeCell", () => {
   it("should render icon on right side of cell if minutes is less than 30", () => {
     const component = render(<TimeCell minutes="40" status="Administered" />);
     expect(component.getByTestId("right-icon")).toBeTruthy();
+  });
+
+  it("should highlight only left side of the cell if highligted flag is true", () => {
+    const component = render(
+      <TimeCell
+        minutes="40"
+        status="Administered"
+        doHighlightCell={true}
+        highlightedCell={"left"}
+      />
+    );
+
+    expect(component.getByTestId("left-icon")).toHaveClass("highligtedCell");
+    expect(component.getByTestId("right-icon")).not.toHaveClass(
+      "highligtedCell"
+    );
+  });
+
+  it("should highlight only right side of the cell if highligted flag is true", () => {
+    const component = render(
+      <TimeCell
+        minutes="40"
+        status="Administered"
+        doHighlightCell={true}
+        highlightedCell={"right"}
+      />
+    );
+
+    expect(component.getByTestId("left-icon")).not.toHaveClass(
+      "highligtedCell"
+    );
+    expect(component.getByTestId("right-icon")).toHaveClass("highligtedCell");
+  });
+
+  it("should not highlight if highligted flag cell is false", () => {
+    const component = render(
+      <TimeCell
+        minutes="40"
+        status="Administered"
+        doHighlightCell={false}
+        highlightedCell={"left"}
+      />
+    );
+
+    expect(component.getByTestId("left-icon")).not.toHaveClass(
+      "highligtedCell"
+    );
+    expect(component.getByTestId("right-icon")).not.toHaveClass(
+      "highligtedCell"
+    );
   });
 });
