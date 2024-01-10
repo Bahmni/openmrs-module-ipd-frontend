262ea6a533b113a5683f0386629b691557b9a3db
tanyaa-tw <120625142+tanyaa-tw@users.noreply.github.com>
2024-01-10 10:19:36 +0530

    Tanya | add test cases

--- src/features/DisplayControls/Treatments/utils/TreatmentsUtils.js
+++ src/features/DisplayControls/Treatments/utils/TreatmentsUtils.js
@@ -43,37 +43,6 @@ export const treatmentHeaders = [
   },
 ];
 
-export const getPrescribedAndActiveDrugOrders = async (
-  patientUuid,
-  numberOfVisits,
-  getOtherActive,
-  visitUuids,
-  startDate,
-  endDate,
-  getEffectiveOrdersOnly
-) => {
-  try {
-    const params = {
-      patientUuid: patientUuid,
-      numberOfVisits: numberOfVisits || 15,
-      getOtherActive: getOtherActive || true,
-      visitUuids: visitUuids || null,
-      startDate: startDate || null,
-      endDate: endDate || null,
-      getEffectiveOrdersOnly: getEffectiveOrdersOnly || false,
-      preferredLocale: getLocale(),
-    };
-    const response = await axios.get(PRESCRIBED_AND_ACTIVE_DRUG_ORDERS_URL, {
-      params: params,
-      withCredentials: true,
-    });
-    if (response.status !== 200) throw new Error(response.statusText);
-    return response.data;
-  } catch (error) {
-    return error;
-  }
-};
-
 export const getAllDrugOrders = async (visitUuid) => {
   try {
     const response = await axios.get(ALL_DRUG_ORDERS_URL(visitUuid), {
--- src/features/DrugChartSlider/tests/DrugChartSlider.spec.jsx
+++ src/features/DrugChartSlider/tests/DrugChartSlider.spec.jsx
@@ -8,12 +8,12 @@ import {
   mockStartTimeFrequencies,
   mockDrugOrderFrequencies,
   mockScheduleFrequenciesWithTimings,
+  mockScheduleDrugOrderForEdit,
 } from "../utils/DrugChartSliderTestUtils";
 import "@testing-library/jest-dom";
 import mockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import { DRUG_ORDERS_CONFIG_URL } from "../../../constants";
-import DrugChartSliderNotification from "../components/DrugChartSliderNotification";
 import MockDate from "mockdate";
 import { SliderContext } from "../../../context/SliderContext";
 
@@ -360,13 +360,29 @@ describe("DrugChartSlider", () => {
     });
     MockDate.reset();
   });
-});
 
-describe("DrugChartSliderNotification", () => {
-  it("Component renders successfully", async () => {
-    const { container } = render(
-      <DrugChartSliderNotification hostData={{}} hostApi={{}} />
+  it.skip("should render with previous time on click of edit drug chart link", async () => {
+    MockDate.set("2010-12-22T07:08:00.000");
+    const { container, getByText, debug } = render(
+      <SliderContext.Provider value={mockSliderContext}>
+        <DrugChartSlider
+          hostData={{
+            enable24HourTimers: true,
+            scheduleFrequencies: mockScheduleFrequenciesWithTimings,
+            startTimeFrequencies: mockStartTimeFrequencies,
+            drugOrder: mockScheduleDrugOrderForEdit,
+          }}
+          hostApi={{}}
+        />
+      </SliderContext.Provider>
     );
-    expect(container).toMatchSnapshot();
+    debug();
+    await waitFor(() => {
+    const startTimeInputs = document.querySelectorAll("#time-selector");
+      expect(startTimeInputs).toBeDefined();
+      console.log("chgtc",startTimeInputs);
+      expect(startTimeInputs[0].value).toBe("hh:mm");
+    });
+    MockDate.reset();
   });
 });
