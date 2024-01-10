07e5db3374cc3c49363ea3b712aa08502a4e5c9a
ayush34957 <97508879+ayush34957@users.noreply.github.com>
2024-01-10 15:47:26 +0530

    feat: updated shift time to 12 hours [Ayush] (#71)

--- src/features/DisplayControls/DrugChart/tests/CalendarHeader.spec.jsx
+++ src/features/DisplayControls/DrugChart/tests/CalendarHeader.spec.jsx
@@ -7,7 +7,7 @@ import MockDate from "mockdate";
 describe("CalendarHeader", () => {
   it("should match snapshot", () => {
     MockDate.set("2023-12-22T00:00:00.000+0530");
-    const currentShiftArray = [14, 15, 16, 17, 18, 19, 20, 21];
+    const currentShiftArray = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
     const { container } = render(
       <CalendarHeader currentShiftArray={currentShiftArray} />
     );
@@ -15,7 +15,7 @@ describe("CalendarHeader", () => {
     MockDate.reset();
   });
   it("should render shiftHours hours", () => {
-    const currentShiftArray = [14, 15, 16, 17, 18, 19, 20, 21];
+    const currentShiftArray = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
     const { getAllByTestId } = render(
       <CalendarHeader currentShiftArray={currentShiftArray} />
     );
