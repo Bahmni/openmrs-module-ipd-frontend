236efa8409917ac1311cf1c0d4edbb916eb4e8d1
srinithishg <srinithish.g@thoughtworks.com>
2024-02-12 11:03:31 +0530

    [Sri] - spike on drugchart left panel

--- src/features/DisplayControls/DrugChart/components/DrugChart.jsx
+++ src/features/DisplayControls/DrugChart/components/DrugChart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import useScrollSync from "react-scroll-sync-hook";
 import Calendar from "./Calendar";
@@ -6,10 +6,14 @@ import CalendarHeader from "./CalendarHeader";
 import "../styles/DrugChart.scss";
 import DrugList from "./DrugList";
 import DrugChartLegend from "../../../../components/AdministrationLegend/AdministrationLegend";
+import { Button } from "carbon-components-react";
+import { ChevronDown20, ChevronUp20 } from "@carbon/icons-react";
 export default function DrugChart(props) {
   const { drugChartData, currentShiftArray, selectedDate } = props;
   const leftPane = useRef(null);
   const rightPane = useRef(null);
+  const [isPrevDisabled, setIsPrevDisabled] = useState(true);
+  const [isNextDisabled, setIsNextDisabled] = useState(false);
   const { registerPane, unregisterPane } = useScrollSync({
     vertical: true,
   });
@@ -32,6 +36,38 @@ export default function DrugChart(props) {
     };
   }, [leftPane, rightPane, registerPane, unregisterPane]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const leftPaneElement = leftPane.current;
+      const scrollTop = leftPaneElement.scrollTop;
+      const maxScrollTop =
+        leftPaneElement.scrollHeight - leftPaneElement.clientHeight;
+
+      setIsPrevDisabled(scrollTop <= 0);
+      setIsNextDisabled(scrollTop >= maxScrollTop);
+    };
+
+    leftPane.current.addEventListener("scroll", handleScroll);
+    return () => {
+      leftPane.current.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = (direction) => {
+    const scrollAmount = 100;
+    const newScrollTop =
+      leftPane.current.scrollTop +
+      (direction === "Prev" ? -scrollAmount : scrollAmount);
+    leftPane.current.scrollTop = newScrollTop;
+    rightPane.current.scrollTop = newScrollTop;
+
+    const scrollTop = newScrollTop;
+    const maxScrollTop =
+      leftPane.current.scrollHeight - leftPane.current.clientHeight;
+    setIsPrevDisabled(scrollTop <= 0);
+    setIsNextDisabled(scrollTop >= maxScrollTop);
+  };
+
   return (
     <div className="drug-chart-dashboard">
       <div className="drug-chart" style={{ height: drugChartHeight }}>
@@ -40,7 +76,14 @@ export default function DrugChart(props) {
           data-testid="left-panel"
           ref={leftPane}
         >
-          <div className="header" />
+          <div className="header">
+            <Button
+              className="arrow-button"
+              renderIcon={ChevronUp20}
+              onClick={() => handleScroll("Prev")}
+              disabled={isPrevDisabled}
+            />
+          </div>
           <DrugList drugDetails={drugChartData} />
         </div>
         <div
@@ -55,6 +98,14 @@ export default function DrugChart(props) {
             selectedDate={selectedDate}
           />
         </div>
+      </div>
+      <div className="arrow-button-wrapper">
+        <Button
+          className="arrow-button"
+          renderIcon={ChevronDown20}
+          onClick={() => handleScroll("Next")}
+          disabled={isNextDisabled}
+        />
       </div>
       <DrugChartLegend />
     </div>
