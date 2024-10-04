f5d5ddb5b9190b36b74d8d6ddfb95ab3e7501b0e
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2024-10-04 14:58:25 +0530

    A - 1208409676255719 | Added Frech support code (#245)

--- src/features/DisplayControls/PatientHeader/components/PatientDetails.jsx
+++ src/features/DisplayControls/PatientHeader/components/PatientDetails.jsx
@@ -1,77 +1,110 @@
-import React from 'react'
-import { Tile, Row, Column} from "carbon-components-react";
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { Tile, Row, Column } from "carbon-components-react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useIntl, FormattedMessage } from "react-intl";
 
-const PatientDetails = ({patientDetails, patientDetailsHeaders, contacts, relationships}) => {
-    const [locationComponent, setLocationComponents]= useState([]);
-    const locationMapping = ()=> {
-        let reveresdAddress = [];
-        for (const key in patientDetails.locations) {
-            if (patientDetails.locations.hasOwnProperty(key)) {
-              let value =  <span className="details-value">
-              {key} : {patientDetails.locations[key] !=null ? patientDetails.locations[key] : " "}
-           </span>
-         setLocationComponents( (locationComponent) => [...locationComponent, value])
-            }
-    }}
-    useEffect(()=>{
-        locationMapping();
-    },[])
+const PatientDetails = ({
+  patientDetails,
+  patientDetailsHeaders,
+  contacts,
+  relationships,
+}) => {
+  const intl = useIntl();
+  const [locationComponent, setLocationComponents] = useState([]);
+  const locationMapping = () => {
+    for (const key in patientDetails.locations) {
+      if (Object.prototype.hasOwnProperty.call(patientDetails.locations, key)) {
+        console.log(" Patient Detials : ", patientDetails.locations[key], key);
+        let value = (
+          <span className="details-value">
+            {intl.messages[key] ? (
+              <FormattedMessage id={key} defaultMessage={key} />
+            ) : (
+              key
+            )}{" "}
+            :{" "}
+            {patientDetails.locations[key] != null
+              ? patientDetails.locations[key]
+              : " "}
+          </span>
+        );
+        setLocationComponents((locationComponent) => [
+          ...locationComponent,
+          value,
+        ]);
+      }
+    }
+  };
+  useEffect(() => {
+    locationMapping();
+  }, []);
+
+  const getLocalizedLabel = (id, defaultLabel) => {
+    return intl.messages[id] ? (
+      <FormattedMessage id={id} defaultMessage={defaultLabel} />
+    ) : (
+      defaultLabel
+    );
+  };
+
   return (
     <Tile className="patient-details">
-        <Row>
-          <Column>
-           <Tile className="details-tile">
-             <span className="details-headers">
+      <Row>
+        <Column>
+          <Tile className="details-tile">
+            <span className="details-headers">
               {patientDetailsHeaders.address}
-             </span>
-           {
-            locationComponent.map((location)=>{
-             return location
-            })
-            }
-           </Tile>
-          </Column>
-          <Column>
-            <Tile className="details-tile">
-                <span className="details-headers">
-                    {patientDetailsHeaders.contactDetails}
+            </span>
+            {locationComponent.map((location) => {
+              return location;
+            })}
+          </Tile>
+        </Column>
+        <Column>
+          <Tile className="details-tile">
+            <span className="details-headers">
+              {patientDetailsHeaders.contactDetails}
+            </span>
+            {contacts.map((contact) => {
+              return (
+                <span key={contact.id} className="details-value">
+                  {getLocalizedLabel(contact.name, contact.name)} :{" "}
+                  {contact.value}
                 </span>
-                {contacts.map((contact)=> { 
-                    return (
-                        <span key={contact.id} className='details-value'>
-                            {contact.name} : {contact.value}
-                        </span>
-                    )
-                })}
-           </Tile>
-          </Column>
-          <Column>
-            <Tile className="details-tile">
-                <span className="details-headers">
-                    {patientDetailsHeaders.relationships}
+              );
+            })}
+          </Tile>
+        </Column>
+        <Column>
+          <Tile className="details-tile">
+            <span className="details-headers">
+              {patientDetailsHeaders.relationships}
+            </span>
+            {relationships.map((relationship) => {
+              return (
+                <span key={relationship.id} className="details-value">
+                  {getLocalizedLabel(
+                    relationship.relationshipType,
+                    relationship.relationshipType
+                  )}{" "}
+                  : {relationship.name}
                 </span>
-                {relationships.map((relationship)=> { 
-                    return (
-                        <span key={relationship.id} className='details-value'>
-                            {relationship.relationshipType} : {relationship.name}
-                        </span>
-                    )
-                })}
-           </Tile>
-          </Column>
-        </Row>
-       </Tile>
-  )
+              );
+            })}
+          </Tile>
+        </Column>
+      </Row>
+    </Tile>
+  );
 };
 
-export default PatientDetails
+export default PatientDetails;
 
 PatientDetails.propTypes = {
-    patientDetails: PropTypes.object.isRequired,
-    patientDetailsHeaders: PropTypes.object.isRequired,
-    contacts: PropTypes.array.isRequired,
-    relationships: PropTypes.array.isRequired
-    };
+  patientDetails: PropTypes.object.isRequired,
+  patientDetailsHeaders: PropTypes.object.isRequired,
+  contacts: PropTypes.array.isRequired,
+  relationships: PropTypes.array.isRequired,
+};
