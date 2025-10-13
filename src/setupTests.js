9be0a22bec4f711d1ff57ea93356b2b496493b0b
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-10-13 11:41:31 +0530

    [Pooja] | Hive-59485 | Fix. NE|IPD| Patient Movement is not working and localised (#261)
    * [Pooja] | Hive-59485 | Fix. NE|IPD| Patient Movement is not working and localised
    
    * [Pooja] | Hive-59485 | Fix. Adding tests for PatientMovementModal

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 // mock the fetch global using jest
 require("jest-fetch-mock").enableMocks();
 
+// Add custom Jest matchers for DOM testing
+import '@testing-library/jest-dom';
+
 // mock out i18n module with __mock__ based files
 jest.mock("./features/i18n/utils");
