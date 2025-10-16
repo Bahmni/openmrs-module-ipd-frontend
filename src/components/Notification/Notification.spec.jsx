9712b82d356159059dbcc413eb5bb5060e0efd88
Nidhilesh PS <nidhilesh.ps@thoughtworks.com>
2025-10-16 13:17:40 +0530

    Nidhilesh| Hive-59223| NE | IPD | French | Add/Modify prescribed medication tab | Fields translated (#266)

--- src/components/Notification/Notification.spec.jsx
+++ src/components/Notification/Notification.spec.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { IntlProvider } from "react-intl";
 import Notification from "./Notification";
+
+const renderWithProviders = (ui) => {
+  return render(
+    <IntlProvider locale="en" messages={{}}>
+      {ui}
+    </IntlProvider>
+  );
+};
 
 describe("Notification", () => {
   it("should show success notification", () => {
-    const { container } = render(
+    const { container } = renderWithProviders(
       <Notification
-        hostData={{ notificationKind: "success" }}
-        hostApi={jest.fn()}
+        hostData={{ notificationKind: "success", messageId: "SUCCESS_MESSAGE" }}
+        hostApi={{}}
       />
     );
     expect(container).toMatchSnapshot();
   });
 
   it("should show warning notification", () => {
-    const { container } = render(
+    const { container } = renderWithProviders(
       <Notification
-        hostData={{ notificationKind: "warning" }}
-        hostApi={jest.fn()}
+        hostData={{ notificationKind: "warning", messageId: "WARNING_MESSAGE" }}
+        hostApi={{}}
       />
     );
     expect(container).toMatchSnapshot();
