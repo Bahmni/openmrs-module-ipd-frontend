674a3055647c0fca6cd6a6cbefad84406a21c782
binduak <tbindu@thoughtworks.com>
2026-06-01 11:17:41 +0530

    Bindu | fix failing test case for amend notes feature

--- src/components/AmendmentHistory/AmendmentHistory.spec.jsx
+++ src/components/AmendmentHistory/AmendmentHistory.spec.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { IntlProvider } from "react-intl";
 import AmendmentHistory from "./AmendmentHistory";
 import PropTypes from "prop-types";
--- src/features/DisplayControls/DrugChart/tests/DrugChartNoteCreation.spec.jsx
+++ src/features/DisplayControls/DrugChart/tests/DrugChartNoteCreation.spec.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import DrugChartNoteCreation from "../components/DrugChartNoteCreation";
 import { IntlProvider } from "react-intl";
 const messages = {
