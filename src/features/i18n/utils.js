0cc013ef56edb4bde5fe1d2c1d13965262f4d2fa
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-06-17 17:42:17 +0530

    BAH-4619 translation fetch from config

--- src/features/i18n/utils.js
+++ src/features/i18n/utils.js
@@ -1,6 +1,9 @@
-import { LS_LANG_KEY, BASE_URL } from "../../constants";
-
-const translationsBaseUrl = "i18n";
+import {
+  LS_LANG_KEY,
+  BASE_URL,
+  CONFIG_TRANSLATIONS_BASE,
+  BUNDLED_TRANSLATIONS_BASE,
+} from "../../constants";
 
 export function getLocale() {
   return localStorage.getItem(LS_LANG_KEY) || "en";
@@ -8,11 +11,20 @@ export function getLocale() {
 
 export const getTranslations = async (locale) => {
   const fileName = `locale_${locale}.json`;
-  return fetchTranslations(fileName);
+  const [bundled, config] = await Promise.all([
+    fetchTranslationFile(`${BASE_URL}${BUNDLED_TRANSLATIONS_BASE}/${fileName}`),
+    fetchTranslationFile(`${CONFIG_TRANSLATIONS_BASE}${fileName}`),
+  ]);
+  return { ...bundled, ...config };
 };
 
-async function fetchTranslations(fileName) {
-  const url = `${BASE_URL}${translationsBaseUrl}/${fileName}`;
-  const response = await fetch(url);
-  return response.json();
+async function fetchTranslationFile(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(response.status);
+    return response.json();
+  } catch (e) {
+    console.error(`Failed to load translations from ${url}:`, e);
+    return {};
+  }
 }
