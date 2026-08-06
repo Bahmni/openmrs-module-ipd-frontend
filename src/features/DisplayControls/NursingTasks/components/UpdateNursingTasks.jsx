b98dc4ceaee3bfabbdb3297e8963f96a2c1a2909
Pooja Sastry <pooja.sastry@thoughtworks.com>
2026-08-06 13:56:41 +0530

    Pooja | Bug-124991 | Unresponsive Save Button in Task Panel (#110)

--- src/features/DisplayControls/NursingTasks/components/UpdateNursingTasks.jsx
+++ src/features/DisplayControls/NursingTasks/components/UpdateNursingTasks.jsx
@@ -334,7 +334,7 @@ const UpdateNursingTasks = (props) => {
           actualTime: checked ? moment() : null,
         },
       });
-      if (!isPRNMedication) {
+      if (!isPRNMedication && !isNonMedication) {
         updateErrors({
           ...errors,
           [id]: Boolean(!tasks[id].notes),
